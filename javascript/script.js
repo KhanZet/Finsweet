8defdf9246ad27aa22790ed2f1d4825eb569db58
KhanZet <umbrazet@yandex.ru>
2022-12-31 20:52:47 +0600

    creating header

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,6 @@
- 
+const burgerButton = document.querySelector('.header__burger-button');
+const header = document.querySelector('.header');
+burgerButton.addEventListener('click', () => {
+	burgerButton.classList.toggle('_active');
+	header.classList.toggle('_active');
+})
